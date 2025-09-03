ea99ad1789c2e8a510a99ee46c80e0dcb27025c7
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-09-03 15:08:45 +0530

    added types file to check types in js

---
+++ fundamentals/types/index.js
@@ -0,0 +1,26 @@
+// Following are the types that are pre defined by js
+
+console.log('type of function is = ',typeof function(){})
+console.log('type of undefined is = ',typeof undefined)
+console.log('type of "a" is = ',typeof 'a')
+console.log('type of null is = ',typeof null)
+console.log('type of window is = ',typeof window)
+console.log('type of Object is = ',typeof Object)
+console.log('type of {} is = ', typeof {})
+console.log('type of [] is = ', typeof [])
+console.log('type of Number is = ',typeof Number)
+console.log('type of String is = ',typeof String)
+console.log('type of Map is = ',typeof Map)
+console.log('type of Set is = ',typeof Set)
+console.log('type of 5 is = ',typeof 5)
+console.log('type of 0 is = ',typeof 0)
+console.log('type of 1 is = ',typeof 1)
+console.log('type of "" is = ', typeof '')
+console.log('type of Boolean is = ',typeof Boolean)
+console.log('type of true is = ',typeof true)
+console.log('type of false is = ',typeof false)
+console.log('type of BigInt is = ',typeof BigInt)
+console.log('type of Array is = ',typeof Array)
+
+
+
